926f6948038cb1f45736e6d009e5ae2e34f893c4
antony <mr_royantony@myyahoo.com>
2025-12-17 13:25:32 +0000

    Initial commit

---
+++ crtprj.sh
@@ -0,0 +1,7 @@
+echo "# hello-gke" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/antony-philip-ai/hello-gke.git
+git push -u origin main
